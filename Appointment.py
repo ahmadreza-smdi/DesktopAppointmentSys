0c0180e93c47170609e8d037f26a6ad364f778ab
Nozhan Amiri <nozhana@gmail.com>
2018-01-12 00:55:21 +0330

    modify Appointment.py

--- Appointment.py
+++ Appointment.py
@@ -1,14 +1,6 @@
-import psycopg2
 import os
-import config
-
-# cls function
-
-def cls():
-    os.system('cls' if os.name is 'nt' else 'clear')
-
-conn = psycopg2.connect('dbname=AppointmentSys user=postgres password=admin')
-cur = conn.cursor()
+from config import *
+import json
 
 ### MENU ###
 
@@ -21,35 +13,69 @@ menu['4'] = 'Update appointment'
 menu['0'] = 'Quit'
 # more options
 
-cls()
 while True:
+    conn.commit()
+    cls()
     for option in menu.items():
         print('\t' + option[0] + '. ' + option[1])
 
     sel = input('\t>_ ')
     if sel is '1':
-        # run add script
+        patient_id = input('\tpatient_id >_ ')
+        appointment_time = input('\tappointment_time >_ ')
+        visit_number = input('\tvisit_number >_ ')
+        payment_roll = input('\tpayment_roll >_ ')
+        insurance_number = input('\tinsurance_number >_ ')
+        insert(patient_id, appointment_time, visit_number, payment_roll, insurance_number)
     elif sel is '2':
-        # run delete script
+        visit_number = input('\tvisit_number >_ ')
+        delete(visit_number)
     elif sel is '3':
-        # run search script
+        visit_number = input('\tvisit_number >_ ')
+        result = search(visit_number)
+        print(result)
     elif sel is '4':
-        # run update script
+        visit_number = input('\tvisit_number >_ ')
+        arg = input('\tcolumn to be updated >_ ')
+        value = input('\tnew value >_ ')
+        update(visit_number, arg, value)
     elif sel is '0':
-        # Quit
+        print('\tTERMINATED')
+        break
     else:
-        # throw exception
+        print('\tWRONG SELECTION.')
 
 ### FUNCTIONS ###
 
 def insert(patient_id, appointment_time, visit_number, payment_roll, insurance_number):
-    # connect to database using CONFIG
+    cur.execute('''
+        INSERT INTO appointment
+        VALUES ({}, {}, {}, {}, {});
+    '''.format(patient_id, appointment_time, visit_number, payment_roll, insurance_number))
+    cur.commit()
 
-def delete(patient_id, appointment_time, visit_number, payment_roll, insurance_number):
-    # connect to database using CONFIG
+def delete(visit_number):
+    cur.execute('''
+        DELETE FROM appointment
+        WHERE visit_number = '{}';
+    '''.format(visit_number))
 
-def search(patient_id, appointment_time, visit_number, payment_roll, insurance_number):
-    # connect to database using CONFIG
+def search(visit_number):
+    cur.execute('''
+        SELECT * FROM appointment
+        WHERE visit_number = '{}';
+    '''.format(visit_number))
+    result = cur.fetchone()
+    return result
 
-def update(patient_id, appointment_time, visit_number, payment_roll, insurance_number):
-    # connect to database using CONFIG
+def update(visit_number, arg, value):
+    cur.execute('''
+        UPDATE appointment
+        SET {} = '{}'
+        WHERE visit_number = '{}';
+    '''.format(arg, value, visit_number))
+
+def insert_n(jcode):
+    rows = json.loads(jcode)
+    for row in rows:
+        insert(row[0], row[1], row[2], row[3], row[4])
