a71ea5e7795261fc6559d8c3beec82deaf89b00b
Ahmadreza samadi <ahmadreza.smdi@gmail.com>
2018-01-11 18:53:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from config import *
 
 global id_number = 0
+
+
 class patient:
     def __init__(self,fname,lname,id_number,national_id
                 ,phone_number,address,plate_number
                 ,post_code,special_disease,blood_type):
+               
                 self.fname = fname
                 self.lname = lname
                 self.id_number = id_number
@@ -16,9 +19,13 @@ class patient:
                 self.special_disease = special_disease
                 self.blood_type = blood_type
 
-			cur.execute(("INSERT INTO patient ( fname, lname,id_number ,national_id,phone_number,address ,plate_number,post_code,special_disease,blood_type)
+			cur.execute(("INSERT INTO patient ( fname, lname,id ,national_id,phone_number,address ,plate_number,post_code,special_disease,blood_type)
                                         VALUES ('%s','%s','%s' ,'%s','%s','%s','%s','%s','%s','%s')")
-				                                %(self.fname, self.lname,self.id_number ,self.national_id,self.phone_number,self.address ,self.plate_number,self.post_code,self.special_disease,self.blood_type))
+				                                %(self.fname, self.lname,self.id_number ,self.national_id,self.phone_number,
+				                                self.address ,self.plate_number,self.post_code,self.special_disease,self.blood_type))
+
+
+
 
 def add_patient():
     print("Please enter the requested elements")
@@ -36,15 +43,24 @@ def add_patient():
             %(fname,lname,id_number,national_id,phone_number,address,plate_number,post_code,special_disease,blood_type))
 
 
+
+
 def del_patient():
     del_id = input("Enter the id that you like to delete:")
-    cur.execute(("DELETE FROM patient WHERE id_number = '%s'")%(del_id))
+    cur.execute(("DELETE FROM patient WHERE id = '%s'")%(del_id))
+
+
+
+
 
 def update_patient():
     up_id = input("Enter the id that you like to update:")
     up_field =input ("What do you want to update ?").lower()
     a = []
-    a = cur.execute(("SELECT '%s' FROM patient WHERE id_number = '%s'")%(del_id,up_field))
+    a = cur.execute(("SELECT '%s' FROM patient WHERE id = '%s'")%(del_id,up_field))
     print("The last value of that was {}".format(a))
     b = input("enter the new value:")
-    cur.execute("UPDATE patient SET '%s' = '%s' WHERE id_number = '%s' ")%(up_field,b,up_id)
+    cur.execute("UPDATE patient SET '%s' = '%s' WHERE id = '%s' ")%(up_field,b,up_id)
+
+
+
