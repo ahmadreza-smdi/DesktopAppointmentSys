ca59e9cf85ce34b1b557233c7a1469944cee6ca8
VahidSa <vahidsanejad@gmail.com>
2018-01-12 16:39:08 +0000

    Advise added

--- Appointment.py
+++ Appointment.py
@@ -10,6 +10,10 @@ menu['1'] = 'Add appointment'
 menu['2'] = 'Delete appointment'
 menu['3'] = 'Lookup appointment'
 menu['4'] = 'Update appointment'
+menu['5'] = 'Add Advise'
+menu['6'] = 'Delete Advise'
+menu['7'] = 'Lookup Advise'
+menu['8'] = 'Update Advise'
 menu['0'] = 'Quit'
 # more options
 
@@ -39,6 +43,25 @@ while True:
         arg = input('\tcolumn to be updated >_ ')
         value = input('\tnew value >_ ')
         update(visit_number, arg, value)
+    elif sel is '5':
+        patient_id = input('patient_id >_ ')
+        visit_number = input('visit_number >_ ')
+        advised = input('advised >_ ')
+        forbidden = input('forbidden >_ ')
+        frowned_upon = input('frowned_upon >_ ')
+        insert_advise(patient_id, visit_number, advised, forbidden, frowned_upon)
+    elif sel is '6':
+        visit_number = input('\tvisit_number >_ ')
+        delete_advise(visit_number)
+    elif sel is '7':
+        visit_number = input('\tvisit_number >_ ')
+        result = search_advise(visit_number)
+        print(result)
+    elif sel is '8':
+        visit_number = input('\tvisit_number >_ ')
+        arg = input('\tcolumn to be updated >_ ')
+        value = input('\tnew value >_ ')
+        update_advise(visit_number, arg, value)
     elif sel is '0':
         print('\tTERMINATED')
         break
@@ -71,6 +94,34 @@ def search(visit_number):
 def update(visit_number, arg, value):
     cur.execute('''
         UPDATE appointment
+        SET {} = '{}'
+        WHERE visit_number = '{}';
+    '''.format(arg, value, visit_number))
+    
+def insert_advise(patient_id, visit_number, advised, forbidden, frowned_upon):
+    cur.execute('''
+        INSERT INTO advise
+        VALUES ({}, {}, {}, {}, {});
+    '''.format(patient_id, visit_number, advised, forbidden, frowned_upon))
+    cur.commit()
+
+def delete_advise(visit_number):
+    cur.execute('''
+        DELETE FROM advise
+        WHERE visit_number = '{}';
+    '''.format(visit_number))
+
+def search_advise(visit_number):
+    cur.execute('''
+        SELECT * FROM advise
+        WHERE visit_number = '{}';
+    '''.format(visit_number))
+    result = cur.fetchone()
+    return result
+
+def update_advise(visit_number, arg, value):
+    cur.execute('''
+        UPDATE advise
         SET {} = '{}'
         WHERE visit_number = '{}';
     '''.format(arg, value, visit_number))
